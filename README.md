fe6bc54b74929b1002e18a900fb14a167085115c
AbyChan <crystal.tyan@gmail.com>
2016-02-17 00:17:56 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # hack-newer
+
+[InAppBrowser](http://cordova.apache.org/docs/en/3.0.0/cordova/inappbrowser/inappbrowser.html#InAppBrowser)
+cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-inappbrowser.git
--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -2,11 +2,14 @@ import {Page, NavController} from 'ionic/ionic';
 import {Http, HTTP_PROVIDERS} from 'angular2/http';
 import  * as _  from 'lodash';
 
+import {timeDifference} from '../../util'
+
 import 'rxjs/add/operator/map';
 
 import './index.scss';
 
 const LATEST_ITEM_N = 10;
+
 
 @Page({
     templateUrl: 'build/pages/index/index.html'
@@ -26,6 +29,14 @@ export class IndexPage {
             );
     }
 
+    openUrl(url) {
+        window.open(url, '_blank', 'location=yes');
+    }
+
+    calcTimeDistance(time) {
+        return timeDifference(new Date().getTime(), time * 1000);
+    }
+    
     handleItems(data) {
         let self = this;
         _.take(data, LATEST_ITEM_N).map((itemId) => {
